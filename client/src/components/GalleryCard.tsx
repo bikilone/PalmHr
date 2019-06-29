f6acc2af19b99ea8e902cd7e87a7eca6b0edfcb2
bikilone <milosbirovljevic@gmail.com>
2019-06-29 17:31:52 +0200

    Added column layout for pictures

--- client/src/components/GalleryCard.tsx
+++ client/src/components/GalleryCard.tsx
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 
 export default class GalleryCard extends Component<{ img: any }> {
   render() {
-    console.log(this.props.img.uri);
-
     return <img src={"http://localhost:5000" + this.props.img.uri} />;
   }
 }
