6b81c2cbdeb36f04121d7036dd20651e082704c5
bikilone <milosbirovljevic@gmail.com>
2019-06-29 13:35:20 +0200

    Installed React Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router";
+import Home from "./containers/Home";
 
 const App: React.FC = () => {
   return (
     <div>
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 };
